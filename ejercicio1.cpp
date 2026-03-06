98871f97c4c0eac3f282e5c1399f7b6ba38fb216
sxlsilent <maldonadobrandon236@gmail.com>
2026-03-06 00:49:08 -0500

    Agregar ejercicio1

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string secret_pin = "0427"; 
+
+    for (int i = 0; i < 10000; i++) {
+        ostringstream oss;
+        oss << setw(4) << setfill('0') << i;
+        string guess = oss.str();
+
+        if (guess == secret_pin) {
+            cout << "PIN encontrado: " << guess << "\n";
+            cout << "Intentos: " << (i + 1) << "\n";
+            break;
+        }
+    }
+
+    return 0;
+}
 
